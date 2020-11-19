879ebeacdda4149ced4840aabbeff1b08438061a
Dongjoo-Kim <kimdj316@naver.com>
2020-11-20 00:39:02 +0900

    update assignment 3

--- 3-IndProp_Imp/E07_01.v
+++ 3-IndProp_Imp/E07_01.v
@@ -3,10 +3,10 @@ Require Import P07.
 Local Opaque aeval beval.
 
 Example test_bevalR1: bevalR (BNot BTrue) false.
-Proof. do 20 try econstructor. Qed.
+Proof. do 20 try econstructor; try eapply my_bevalR1. Qed.
 Example test_bevalR2: bevalR (BEq (APlus (ANum 2) (ANum 1)) (ANum 3)) true.
-Proof. do 20 try econstructor. Qed.
+Proof. do 20 try econstructor; try eapply my_bevalR2. Qed.
 Example test_bevalR3: bevalR (BAnd (BLe (AMult (ANum 3) (ANum 1))
                                         (AMinus (ANum 1) (ANum 3)))
                                    BTrue) false.
-Proof. do 20 try econstructor. Qed.
+Proof. do 20 try econstructor; try eapply my_bevalR3. Qed.
--- 3-IndProp_Imp/P07.v
+++ 3-IndProp_Imp/P07.v
@@ -9,6 +9,21 @@ Inductive bevalR: bexp -> bool -> Prop :=
   (* FILL_IN_HERE *)
 .
 
+(** If your definition can't proof these examples using "do 20 try econstructor", **)
+(** comment out and write your proofs in following area. **)
+
+(*
+Example my_bevalR1: bevalR (BNot BTrue) false.
+Proof. do 20 try econstructor. Qed.
+Example my_bevalR2: bevalR (BEq (APlus (ANum 2) (ANum 1)) (ANum 3)) true.
+Proof. do 20 try econstructor. Qed.
+Example test_bevalR3: bevalR (BAnd (BLe (AMult (ANum 3) (ANum 1))
+                                        (AMinus (ANum 1) (ANum 3)))
+                                   BTrue) false.
+Proof. do 20 try econstructor. Qed.
+*)
+
+
 Check aeval_iff_aevalR.
 
 Lemma beval_iff_bevalR : forall b bv,
