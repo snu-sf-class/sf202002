674d0a67d1b7b8f6fe155b148946add28b2ba643
Gil Hur <gil.hur@sf.snu.ac.kr>
2020-12-20 10:35:18 +0900

    Update the hauto tactics

--- hauto.v
+++ hauto.v
@@ -11,31 +11,38 @@ Ltac hauto_vc :=
   eauto;
   unfold "->>", assn_sub, t_update, bassn;
   intros; simpl in *;
-  repeat match goal with [H: _ /\ _ |- _] => destruct H end;
+  repeat
+    match goal with
+    | [H: _ <> true |- _] => apply not_true_iff_false in H
+    | [H: _ <> false |- _] => apply not_false_iff_true in H
+    | [H: _ /\ _ |- _] => destruct H
+    | [H: _ && _ = true |- _] => apply andb_true_iff in H
+    | [H: _ && _ = false |- _] => apply andb_false_iff in H
+    | [H: _ || _ = true |- _] => apply orb_true_iff in H
+    | [H: _ || _ = false |- _] => apply orb_false_iff in H
+    | [H: negb _ = true |- _] => eapply negb_true_iff in H
+    | [H: negb _ = false |- _] => eapply negb_false_iff in H
+    | [H: (_ =? _) = true |- _] => eapply beq_nat_true in H
+    | [H: (_ =? _) = false |- _] => eapply beq_nat_false in H
+    end;
   repeat (
-      try rewrite -> eqb_eq in *;
-      try rewrite -> leb_le in *;
-      try rewrite leb_iff in *;
-      try rewrite leb_iff_conv in *;
-      try match goal with [H: negb _ <> true |- _] => eapply not_true_is_false in H end;
-      try match goal with [H: negb _ <> false |- _] => eapply not_false_is_true in H end;
-      try match goal with [H: negb _ = true |- _] => eapply negb_true_iff in H end;
-      try match goal with [H: negb _ = false |- _] => eapply negb_false_iff in H end;
-      try match goal with [H: (_ =? _) = true |- _] => eapply beq_nat_true in H end;
-      try match goal with [H: (_ =? _) = false |- _] => eapply beq_nat_false in H end
-    );
-  try contradiction; eauto; try nia.
+    try rewrite -> eqb_eq in *;
+    try rewrite -> leb_le in *;
+    try rewrite leb_iff in *;
+    try rewrite leb_iff_conv in *
+  );
+  try discriminate; try contradiction; eauto; try nia.
 
 Ltac hauto_split1 :=
   try match goal with
       | [|- {{_}} skip {{_}}] =>
-        first [eapply hoare_skip;[] |eapply hoare_consequence_pre; [eapply hoare_skip|]]
+        first [eapply hoare_skip; fail | eapply hoare_consequence_pre; [eapply hoare_skip|]]
       | [|- {{_}} _ := _ {{_}}] =>
-        first [eapply hoare_asgn;[] |eapply hoare_consequence_pre; [eapply hoare_asgn|]]
+        first [eapply hoare_asgn;[] | eapply hoare_consequence_pre; [eapply hoare_asgn|]]
       | [|- {{_}} _; _ {{_}}] =>
         eapply hoare_seq
       | [|- {{_}} if _ then _ else _ end {{_}}] =>
-        first [eapply hoare_if_wp;[|] |eapply hoare_consequence_pre; [eapply hoare_if_wp|]]
+        first [eapply hoare_if_wp;[|] | eapply hoare_consequence_pre; [eapply hoare_if_wp|]]
       end.
 
 Ltac hauto :=
